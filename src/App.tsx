9da031547bcaceab04579ce6cc5ab892c9c7ce8a
Sean Pheavyraksonya <seanphevyraksonya@gmail.com>
2025-08-21 14:44:06 +0700

    import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Card from "./components/card";
 const App = () => {
   return (
     <div>
       <h1>Hello</h1>
       <p>jong yy tha jong yy jg ha</p>
+      <Card />
     </div>
   );
 };
---
+++ src/components/card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <h1>kdm ah jmr</h1>
+    </div>
+  );
+};
+
+export default card;
